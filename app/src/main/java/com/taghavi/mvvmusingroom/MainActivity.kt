aaa10b3af8836b377ab12a6514f350b0ce74a924
EhsanTgv <EhsanTgv@gmail.com>
2021-01-31 23:00:07 +0330

    Initial commit

---
+++ app/src/main/java/com/taghavi/mvvmusingroom/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taghavi.mvvmusingroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
