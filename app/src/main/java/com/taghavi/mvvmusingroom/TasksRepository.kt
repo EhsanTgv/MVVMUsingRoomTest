253dfee26c789f15737ca4a3c268e306cc495730
EhsanTgv <EhsanTgv@gmail.com>
2021-02-02 23:08:58 +0330

    Implement TasksRepository class

---
+++ app/src/main/java/com/taghavi/mvvmusingroom/TasksRepository.kt
@@ -0,0 +1,30 @@
+package com.taghavi.mvvmusingroom
+
+import androidx.lifecycle.LiveData
+
+
+class TasksRepository(var database: AppDatabase) {
+    private var tasks: LiveData<List<TaskEntry?>?>? = null
+    private val taskDao: TaskDao? = null
+    fun getloadAllTasks(): LiveData<List<TaskEntry?>?>? {
+        tasks = database.taskDao()!!.loadAllTasks()
+        return tasks
+    }
+
+    fun getloadTaskById(taskId: Int): LiveData<TaskEntry?>? {
+        return database.taskDao()!!.loadTaskById(taskId)
+    }
+
+    fun deleteTasks(taskEntry: TaskEntry?) {
+        database.taskDao()!!.deleteTask(taskEntry)
+    }
+
+    fun updateTaskById(task: TaskEntry?) {
+        database.taskDao()!!.updateTask(task)
+    }
+
+    companion object {
+        private val LOG_TAG = TasksRepository::class.java
+            .simpleName
+    }
+}
